d04289e055fcf7c054bb2de47251420fd9f3112c
Sean Kelly <invertedrdm@gmail.com>
2021-05-11 05:08:22 -0400

    more tweaking

--- server.js
+++ server.js
@@ -13,10 +13,16 @@ if (process.env.NODE_ENV === "production") {
 
 app.use(routes);
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/googlebooks',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
+
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
